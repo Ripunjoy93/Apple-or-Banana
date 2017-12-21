c66d1b0303d5d639051ce98d1356136ff5562c8d
Ripunjoy Gohain <ripunjoy.gohain@widas.in>
2017-12-21 11:14:11 +0530

    First commit

---
+++ src/main/java/spark_ml/apple_or_banana/ImageClassifier/App.java
@@ -0,0 +1,13 @@
+package spark_ml.apple_or_banana.ImageClassifier;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
